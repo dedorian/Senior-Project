b2a53985cafd6f9a7c4a985bf073214b7dc2eda3
Patrick <patrick.j.king@gmail.com>
2017-06-13 22:16:50 -0500

    Still troubleshooting. I kind of hate the devise gem right now, though I'm sure it's probably something I did to make it mad.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update]
   resources :orders
   resources :messages
   resources :products
 
   get "users/register", as: 'signup'
-  get "users/edit", as 'edit_account'
   get "support/index", as: 'support'
   root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
