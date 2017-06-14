e50c8832bda959a4e33b6d60cd8645fb1a155951
Patrick <patrick.j.king@gmail.com>
2017-06-13 23:03:10 -0500

    Added User controller/views. Maybe I was missing a handler?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users
   resources :orders
   resources :messages
   resources :products
