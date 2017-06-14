f83f3812c08b8c876ac0df2c6a41206e777e42be
Patrick <patrick.j.king@gmail.com>
2017-06-13 21:38:33 -0500

    Troubleshooting registration still. Updated Orders and Support views (UX and Admin controls).

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {
-    sign_in: 'login', sign_out: 'logout',
-    password: 'secret', confirmation: 'verification',
-    registration: 'register', edit: 'edit/profile'
-    }
+  devise_for :users
   resources :orders
   resources :messages
   resources :products
