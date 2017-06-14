2149a6f0f99582dbabba1f3a09f7de95db335d17
Patrick <patrick.j.king@gmail.com>
2017-06-13 21:02:17 -0500

    Trying to fix the edit user path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    password: 'secret', confirmation: 'verification',
+    registration: 'register', edit: 'edit/profile'
+    }
   resources :orders
   resources :messages
   resources :products
