e3cbc63b9eb2b7c6949d99871c4c70196476010b
Patrick <patrick.j.king@gmail.com>
2017-06-13 23:28:17 -0500

    Jeeze. Mistype.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,2 @@
 class UserController < ApplicationController
-    # GET /orders
-    # GET /orders.json
-    def index
-      @users = User.all
-    end
-
-    # GET /orders/1
-    # GET /orders/1.json
-    def show
-    end
-
-    # GET /orders/new
-    def new
-      @user = User.new
-    end
-
-    # GET /orders/1/edit
-    def edit
-    end
-
 end
