c80c0a51c649c2e96d7cbf534fa05b16c79509c6
Patrick <patrick.j.king@gmail.com>
2017-06-14 22:41:41 -0500

    Explicit routes... again. There are a LOT of different schools of thought as to which of these should work and, thus far, none have. Let's see.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :messages
   resources :products
 
+  as :user do
+    get "edit_account", to: "devise/registrations#edit"
+  end
+
   get "support/index", as: 'support'
   root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
