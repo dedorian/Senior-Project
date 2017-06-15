00625f2c6b3650b5f80aff4b9582019f9764763b
Patrick <patrick.j.king@gmail.com>
2017-06-14 22:17:44 -0500

    Undoing last change because why not.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   resources :messages
   resources :products
 
-  devise_scope :user do
-    get 'users/:id' => 'registrations#show', as: user
-  end
-
   get "support/index", as: 'support'
   root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
