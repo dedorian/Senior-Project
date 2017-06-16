5a8e505f9917dc7e13f4970ce6ca7272ade508d4
Patrick <patrick.j.king@gmail.com>
2017-06-16 10:54:57 -0500

    Deleted erroneous controller (let's hope that doesn't break anything), updated some UX on the product show views and changed `edit account` to `edit password` to troubleshoot routing issues.

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-    skip_before_filter :verify_authenticity_token, :only => :create
-
-    def edit
-        user = User.find(params[:id])
-    end
-
-end
