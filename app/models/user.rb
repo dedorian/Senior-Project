9f74ffec0004b98878ad86bcb99a57a2def1c0cf
Patrick <patrick.j.king@gmail.com>
2017-06-16 13:57:09 -0500

    Validation error because I followed a decade-old document instead of the current stuff. Whelp.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-    validate_on_create :validate_card
+    validate :validate_card
 
 private
 
