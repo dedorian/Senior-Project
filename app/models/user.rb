445cfbfcb584b1a45de987a47ea8caece7f5900b
Patrick <patrick.j.king@gmail.com>
2017-06-16 14:31:05 -0500

    Today is definitely a long day.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,29 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
     validate :validate_card, :on => :create
+    attr_accessor :card_number, :card_verification
 
 private
 
-    unless credit_card.valid?
-        credit_card.errors.full_messages.each do |message|
-            errors.add_to_base message
+    def validate_card
+        unless credit_card.valid?
+            credit_card.errors.full_messages.each do |message|
+                errors.add_to_base message
+            end
         end
     end
 
     def credit_card
         @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
-            type: card_type,
-            number: card_number,
-            verification_value: card_verification,
-            month: card_expires_on.month,
-            year: card_expires_on.year,
-            first_name: first_name,
-            last_name: last_name
+            :type => card_type,
+            :number => card_number,
+            :verification_value => card_verification,
+            :month => card_expires_on.month,
+            :year => card_expires_on.year,
+            :first_name => first_name,
+            :last_name => last_name
         )
     end
+
+
 end
