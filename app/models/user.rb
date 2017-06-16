70ea9276b52b2e80c1070682a7a954b023556847
Patrick <patrick.j.king@gmail.com>
2017-06-16 13:11:02 -0500

    What the hell, me?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    require 'active_merchant'
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+    validate_on_create :validate_card
+
+private
+
+    unless credit_card.valid?
+        credit_card.errors.full_messages.each do |message|
+            errors.add_to_base message
+        end
+    end
+
+    def credit_card
+        @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
+            type: card_type,
+            number: card_number,
+            verification_value: card_verification,
+            month: card_expires_on.month,
+            year: card_expires_on.year,
+            first_name: first_name,
+            last_name: last_name
+        )
+    end
 end
