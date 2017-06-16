b7b8a8507487c16359597af3c16cbee04beecf01
Patrick <patrick.j.king@gmail.com>
2017-06-16 14:45:08 -0500

    ... long day.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    require 'active_merchant'
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
