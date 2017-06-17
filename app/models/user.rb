ce67ce2c3f6a632bd2c87d2a8719b78c35a31904
Patrick <patrick.j.king@gmail.com>
2017-06-16 19:19:31 -0500

    Added new user validation. Testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,19 @@ class User < ApplicationRecord
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+
     validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :street, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
+
+    validates :card_number, presence: true, numericality: true, length: { is: 16 }
+    validates :card_verification, presence: true, numericality: true
 
     attr_accessor :card_number, :card_verification, :card_type, :card_expires_on
 
