31bcabb2df9d4200e76aac046c8490e8d536b15a
Patrick <patrick.j.king@gmail.com>
2017-06-16 19:03:40 -0500

    Testing validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-    require 'active_merchant'
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+    validates :first_name, presence: true
 
     attr_accessor :card_number, :card_verification, :card_type, :card_expires_on
 
