0bd535208b701d0434de8d4ee92fc099c35af603
Patrick <patrick.j.king@gmail.com>
2017-06-18 18:40:51 -0500

    Same.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,9 +65,9 @@ class OrdersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
+    #def set_order
+    #  @order = Order.find(params[:id])
+    #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
