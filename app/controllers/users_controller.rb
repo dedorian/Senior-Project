8ba1cfefe53881511c2ba34e63e2b43e9a2788b4
Patrick <patrick.j.king@gmail.com>
2017-06-18 22:48:47 -0500

    Bug killing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,11 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+    def destroy
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to orders_url, notice: "The user's account was deleted." }
+      end
+    end
 end
