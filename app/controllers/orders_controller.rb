2575d3d5e2499e7865a36a8a204441479f514d29
Patrick <patrick.j.king@gmail.com>
2017-06-18 22:04:13 -0500

    Syntax still.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,9 +9,9 @@ class OrdersController < ApplicationController
             if current_user.admin?
                 @orders = Order.all
             end
-            @orders = Order.where(order.user => current_user.email)
+            @orders = Order.where(@order.user => current_user.email)
         else
-            format.html { redirect_to root_path, notice: 'You have to be logged in to view your orders!' }
+            redirect_to root_path, notice: 'You have to be logged in to view your orders!'
         end
     end
 
